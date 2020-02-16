0e0368678cfbce8a3a614a2019ee4e471dc78af3
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-02-16 19:11:01 +0200

    Footer Component

--- Squins/front/sequins/src/pages/about/about.js
+++ Squins/front/sequins/src/pages/about/about.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/header/header";
 import Slider from "../../components/about-slider/about-slider";
+import Footer from "../../components/Footer/Footer";
 export default function About(){
     return (
       <>
       <Header/>
       <Slider/>
+      <Footer />
       </>
     );
   }
--- Squins/front/sequins/src/pages/home/home.js
+++ Squins/front/sequins/src/pages/home/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/header/header";
 import Collection from "../../components/collection/collection";
 import Slider from "../../components/Slider/slider";
+import Footer from "../../components/Footer/Footer";
 export default function Home(){
     return (
       <>
@@ -9,6 +10,7 @@ export default function Home(){
       <Header />
       <Slider value="second"/>
       <Collection />
+      <Footer />
      
       </>
     );
